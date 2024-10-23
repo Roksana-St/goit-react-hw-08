4b799eb47acb270e6b9dc55a5c24ad10e145103a
Roksana St <160417317+Roksana-St@users.noreply.github.com>
2024-10-23 13:37:14 +0300

    upgrade hw 8

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from '../redux/contacts/operations'; 
+import { selectContacts, selectLoading, selectError } from '../redux/contacts/selectors'; 
 import ContactForm from '../components/ContactForm/ContactForm';
 import ContactList from '../components/ContactList/ContactList';
 import SearchBox from '../components/SearchBox/SearchBox';
 
 const ContactsPage = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts);
+  const isLoading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div>
       <h1>Your Contacts</h1>
       <ContactForm />
       <SearchBox />
-      <ContactList />
+      {isLoading && <p>Loading contacts...</p>}
+      {error && <p>Error: {error}</p>}
+      {contacts.length > 0 ? (
+        <ContactList />
+      ) : (
+        <p>No contacts available. Please add a contact.</p>
+      )}
     </div>
   );
 };
